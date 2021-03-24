90d45442966c4f28fbb9b1152e20c517b1376e50
aashish2000 <aash.ananth@gmail.com>
2021-03-25 00:54:01 +0530

    Update README.md and add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # Parking_Management
 Parking Management System
+
+## Table of contents
+* [About](#about)
+* [Features](#features)
+* [Packages Used](#packages-used)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Screenshots](#screenshots)
+* [References](#references)
+
+## About
+
+This project aims at efficiently handling vehicle parking services across parking lots. Vehicles are uniquely identified using their number plate information and tracked across camera locations. The location of a parked car is monitored for easy reference and access. Parking slot vacancy is also computed for managing the parking capacity of a parking lot. Focus has been placed in making the system robust for handling low-light surroundings and unconstrained camera angles.
+
+## Features
+
+- Vehicle Detection Models are based on the YOLOv4 architecture executed using the darknet framework
+- ALPR is handled using a custom Keras Model for Licence Plate Detection and a YOLO based Detector for performing Numberplate character OCR
+
+## Packages Used
+- TensorFlow - 1.15.4
+- Keras - 2.2.4
+- FastAPI
+- OpenCV
+- imutils
+- nest-asyncio
+- pyngrok
+- asgiref
+
+## Setup
+
+### Requirements
+- Python 3.6+
+
+### Installation
+- To install required packages:<br>
+```bash
+pip install -r requirements.txt
+```
+
+- For CPU Only execution, do the corresponding changes in ```./darknet/Makefile```
+```
+GPU = 0
+CUDNN = 0
+```
+
+- Building YOLO Darknet Binaries
+```bash
+cd darknet && make
+```
+
+## Usage
+
+```bash
+python car_numberplate_recognition.py $IMAGE_DIR
+```
+
+## References
+
+- Vehicle Detection: <https://arxiv.org/abs/2011.04244>
+- Licence Plate Detection in Unconstrained Scenes: <http://sergiomsilva.com/pubs/alpr-unconstrained/>
+
+
--- car_numberplate_recognition.py
+++ car_numberplate_recognition.py
@@ -6,15 +6,16 @@ from src.keras_utils 		import load_model, detect_lp
 import imutils
 import darknet.darknet
 import numpy as np
+import sys
 from copy import deepcopy
 import cv2
 import random
 
 def bbox2points(bbox):
-    """
+    '''
     From bounding box yolo format
     to corner points cv2 rectangle
-    """
+    '''
     x, y, w, h = bbox
     xmin = int(round(x - (w / 2)))
     xmax = int(round(x + (w / 2)))
@@ -46,6 +47,10 @@ def resize_frame(input_frame,dimensions):
 
 
 def vehicle_detection(image_name, vehicle_net, vehicle_meta, vehicle_threshold):
+    '''
+    Use darknet framework for identifying vehicle bounding boxes
+    in image frame and cropping them for licence plate detection
+    '''
     detections , _ = darknet.darknet.detect(vehicle_net, vehicle_meta, bytes(image_name, encoding='utf-8') ,thresh=vehicle_threshold)
 
     detections = [r for r in detections if r[0] in [b'car', b'bus', b"motorbike", b"truck"]]
@@ -55,8 +60,14 @@ def vehicle_detection(image_name, vehicle_net, vehicle_meta, vehicle_threshold):
     return(detections)
 
 def licence_plate_detection(image_original, cropped_car, wpod_net, lp_threshold):
+    '''
+    Returns image crop containing the number plate of a vehicle 
+    '''
+
     Iorig = deepcopy(image_original)
     original_width = np.array(Iorig.shape[1::-1],dtype=float)
+
+    # Extracting image crop containing a vehicle from camera frame
     cx,cy,w,h = (np.array(cropped_car[2])/np.concatenate( (original_width,original_width))).tolist()
     tl = np.array([cx - w/2., cy - h/2.])
     br = np.array([cx + w/2., cy + h/2.])
@@ -64,16 +75,22 @@ def licence_plate_detection(image_original, cropped_car, wpod_net, lp_threshold)
     Ivehicle = crop_region(Iorig,label)
     Ivehicle = Ivehicle.astype(np.uint8)
 
+    # Computing vehicle dimensions relative to image crop
     ratio = float(max(Ivehicle.shape[:2]))/min(Ivehicle.shape[:2])
     side  = int(ratio*288.)
     bound_dim = min(side + (side%(2**4)),608)
     print("\t\tBound dim: %d, ratio: %f" % (bound_dim,ratio))
-
+    
+    # Run model inference for returning licence plate bounding boxes
     licence_coords, licence_images ,_ = detect_lp(wpod_net, im2single(Ivehicle), bound_dim, 2**4, (240,80), lp_threshold)
 
     return(licence_coords, licence_images)
 
 def licence_plate_ocr(licence_image, ocr_net, ocr_meta, ocr_threshold, ocr_net_width, ocr_net_height):
+    '''
+    Extracts characters of the licence plate and orders them
+    to return the predicted licence plate of a vehicle
+    '''
     licence_image *= 255.
     image_resized = cv2.resize(licence_image, (ocr_net_width, ocr_net_height), interpolation=cv2.INTER_NEAREST)
     cv2.imwrite("./tmp/file.jpg", image_resized)
@@ -90,6 +107,7 @@ def licence_plate_ocr(licence_image, ocr_net, ocr_meta, ocr_threshold, ocr_net_w
         print("LICENCE_PLATE: ", lp_str)
 
 
+# Initialize Vehicle Detection Model
 vehicle_threshold = .5
 vehicle_weights = b'../yolov4/yolov4-tiny.weights'
 vehicle_netcfg  = b'../yolov4/yolov4-tiny.cfg'
@@ -98,9 +116,11 @@ vehicle_dataset = b'./darknet/cfg/coco.data'
 vehicle_net  = darknet.darknet.load_net(vehicle_netcfg, vehicle_weights, 0)
 vehicle_meta = darknet.darknet.load_meta(vehicle_dataset)
 
+#Initialize Licence Plate Detection model
 lp_threshold = .5
 wpod_net = load_model("./data/lp-detector/wpod-net_update1.h5")
 
+# Initiailze Licence Plate OCR Model
 ocr_threshold = .4
 ocr_weights = b'data/ocr/ocr-net.weights'
 ocr_netcfg  = b'data/ocr/ocr-net.cfg'
@@ -111,28 +131,34 @@ ocr_meta = darknet.darknet.load_meta(ocr_dataset)
 ocr_net_width = darknet.darknet.network_width(ocr_net)
 ocr_net_height = darknet.darknet.network_height(ocr_net)
 
-images = load_images("./test_images")
+# Load test images from input directory
+images = load_images(sys.argv[1])
 width = darknet.darknet.network_width(vehicle_net)
 height = darknet.darknet.network_width(vehicle_net)
+
 
 for index in range(len(images)):
     image_name = images[index]
     image_original = cv2.imread(image_name)
     print(image_name)
 
+    # Perform Vehicle Detection
     detected_cars = vehicle_detection(image_name, vehicle_net, vehicle_meta, vehicle_threshold)
 
     if len(detected_cars):
         for i, cropped_car in enumerate(detected_cars):
+            # Perform Licence Plate Detection for every detected vehicle
             licence_coords, licence_images = licence_plate_detection(image_original, cropped_car, wpod_net, lp_threshold)
             
             if len(licence_coords):
                 licence_plate = licence_images[0]
                 
-                cv2.imshow("window",licence_plate)
-                if cv2.waitKey() & 0xFF == ord('q'):
-                    break
+                # Display Detected Licence Plate
+                # cv2.imshow("window",licence_plate)
+                # if cv2.waitKey() & 0xFF == ord('q'):
+                #     break
 
+                # Perform Licence Plate Character Recognition
                 licence_plate_ocr(licence_plate, ocr_net, ocr_meta, ocr_threshold, ocr_net_width, ocr_net_height)
 
 
